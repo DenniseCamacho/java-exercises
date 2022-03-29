480b1fae4767604b3fb1f8d23dabefa2681532a6
Dennise Camacho <98361785+DenniseCamacho@users.noreply.github.com>
2022-03-29 11:55:47 -0500

    Control Statements and Loops Exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -128,14 +128,15 @@ public class ControlFlowExercises {
         //2.now you can get users input.
         System.out.println("Please enter a number: ");
         int usersNumber = sc.nextInt();
+**/
 //     TODO:   Display a table of squares and cubes from 1 to the value entered.
+/**
         long usersNumberSquared = usersNumber * usersNumber;
         long usersNumberCubed = usersNumber * usersNumber * usersNumber;
         for (int i = 1; i < usersNumber; i++) {
             System.out.printf("%d squared is: " + usersNumberSquared, usersNumber);
         }
-
-  **/
+**/
 //        Ask if the user wants to continue.
 //        Assume that the user will enter valid data.
 //        Only continue if the user agrees to.
