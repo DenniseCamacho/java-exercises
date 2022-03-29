f5200665df98c8e53437855f8b2a36279e519c92
Dennise Camacho <98361785+DenniseCamacho@users.noreply.github.com>
2022-03-29 15:38:50 -0500

    Java string exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    //    TODO:    Create a class named StringExercise with a main method.
+    public static void main(String[] args) {
+//        String Basics.
+//TODO:For each of the following output examples: create a String variable that contains
+// the desired output and print it out to the console, you can do this with only one
+// String variable and one print statement for each output example.
+//                "We don't need no education
+//        We don't need no thought control"
+        /**
+         String message1 = "We don't need no education \nWe don't need no thought control";
+         System.out.println(message1);
+         **/
+//  TODO:      Check "this" out!, "s inside of "s!
+        /**
+         String message2 = "Check \"this\" out!, \"s inside of \"s!";
+         System.out.println(message2);
+         **/
+//  TODO:          In windows, the main drive is usually C:\
+        /**
+         String message3 = "In windows, the main drive is usually C:\\";
+         System.out.println(message3);
+         **/
+//  TODO:      I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+        /**
+         String message4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+         "and the amazing triple backslash \\\\\\!";
+         System.out.println(message4);
+         **/
+    }
+}
+class Bob {
+    public static void main(String[] args) {
+//  TODO: [x]Create a class named Bob with a main method for the following exercise.
+//        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//        []Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//        []He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//        []He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//        []He answers 'Whatever.' to anything else.
+        //create Strings with Bob's responses(4)
+        String messageWithQuestionMark = "Sure.";
+        String messageWithExclamation = "Whoa, chill out!";
+        String messageWithNothing = "Fine. Be that way!";
+        String messageWithAnythingElse = "Whatever.";
+        Scanner sc = new Scanner(System.in);
+        //create a Scanner.
+
+
+//        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+    }
+}
