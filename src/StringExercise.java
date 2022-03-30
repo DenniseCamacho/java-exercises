8a6848667df25388b1181074d5e78f50a6514cec
Dennise Camacho <98361785+DenniseCamacho@users.noreply.github.com>
2022-03-29 19:32:43 -0500

    Java string exercises, final exercise. Need to make it loop...

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -36,19 +36,73 @@ class Bob {
     public static void main(String[] args) {
 //  TODO: [x]Create a class named Bob with a main method for the following exercise.
 //        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-//        []Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-//        []He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-//        []He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-//        []He answers 'Whatever.' to anything else.
+//        Bob answers 'Sure.' if you ask him a question.
+//        [x](the input ends with a question mark)
+//        He answers 'Whoa, chill out!' if you yell at him.
+//        [x](the input ends with an exclamation mark)
+//        He says 'Fine. Be that way!' if you address him without actually saying anything.
+//        [x](empty input)
+//        [x]He answers 'Whatever.' to anything else.
+//        Write the Java code necessary so that a user of your
+//        command line application can have a conversation with Bob.
         //create Strings with Bob's responses(4)
         String messageWithQuestionMark = "Sure.";
         String messageWithExclamation = "Whoa, chill out!";
         String messageWithNothing = "Fine. Be that way!";
         String messageWithAnythingElse = "Whatever.";
-        Scanner sc = new Scanner(System.in);
+        String contQuestion = "Do you still want to talk to Bob?";
         //create a Scanner.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to talk to Bob? yes/no");
+        String usersResponse = sc.nextLine();
+        String continues;
+        switch(usersResponse) {
+            case "no":
+                System.out.println("Bob: ...");
+                break;
+            case "yes":
+                System.out.println("What would you like to say?");
+                System.out.println("Bob: ...");
+                String usersResponse2 = sc.nextLine();
+                if (usersResponse2.indexOf("?") >= 0){
+                    System.out.println(messageWithQuestionMark);
+                } else if (usersResponse2.indexOf("!") >= 0){
+                    System.out.println(messageWithExclamation);
+                } else if (usersResponse2.length() < 1){
+                    System.out.println(messageWithNothing);
+                } else {
+                    System.out.println(messageWithAnythingElse);
+                }
+                System.out.println(contQuestion);
+                continues = sc.nextLine();
+        }
 
 
-//        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+
+
+
+
+
+
+//       if (usersResponse.equals("no")){
+//               System.out.println("He's busy anyway.");
+//           } else if (usersResponse.equals("yes")) {
+//       }
+//        String usersResponse2 = sc.nextLine();
+//       if (usersResponse2.equals("hi")){
+//
+//       }
+
+
+//            System.out.println("\n Please enter your name, favorite color and favorite food");
+//            String usersResponse = sc.next();
+//            if (usersResponse.equals()) {
+//                System.out.println(messageWithNothing);
+
+        //(if the input ends with a question mark)
+        //(the input ends with an exclamation mark)
+        //(empty input)
+        //'Whatever.' to anything else.
+
     }
 }
