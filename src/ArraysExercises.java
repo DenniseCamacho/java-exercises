ecb7fa7453b40b54fc03044b802c83379cc89e32
New Author Name <dennise.e.camacho@gmail.com>
2022-04-04 12:02:27 -0500

    Arrays exercises section 1 progress...//after accidental deleting

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -9,6 +9,7 @@ public class ArraysExercises {
 //
 //      What happens when you run the following code? Why is
 //      Arrays.toString necessary? it shows the array, not the #
+         /**
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(
         "without Arrays.toString(): " +
@@ -17,23 +18,29 @@ public class ArraysExercises {
         "with Arrays.toString(): " +
         Arrays.toString(numbers)
         );
+         **/
 //      Create an array that holds 3 Person objects. Assign a new
 //      instance of the Person class to each element. Iterate through
 //      the array and print out the name of each person in the array.
+         /**
         String[] threePeople = {"Daniel", "David", "Elijah"};
         for (String onePerson : threePeople) {
             System.out.println("this is forloop: " + onePerson);
         }
         addPerson(Arrays.toString(threePeople));
         }
+         **/
 //      Create a static method named addPerson. It should
 //      accept an array of Person objects, as well as a
 //      single person object to add to the passed array. It
 //      should return an array whose length is 1 greater than
 //      the passed array, with the passed person object at
 //      the end of the array.
-    public static String addPerson(String a){
-        return a;
+    /**
+    public static void addPerson(String a){
+        System.out.println(Arrays.toString(a).length());
     }
+     **/
 
     }
+}
