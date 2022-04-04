1e1732a43001efe850a084591a9dafd2f5cec570
New Author Name <dennise.e.camacho@gmail.com>
2022-04-04 12:31:09 -0500

    Arrays exercises review

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -37,10 +37,48 @@ public class ArraysExercises {
 //      the passed array, with the passed person object at
 //      the end of the array.
     /**
-    public static void addPerson(String a){
-        System.out.println(Arrays.toString(a).length());
-    }
+
+     Person[] persons = new Person[3];
+     persons[0] = new Person("Justin");
+     persons[1] = new Person("Dane");
+     persons[2] = new Person("Javier");
+
+     for (Person person : persons){
+     System.out.println(person.getName());
+     }
+     Person[] newPersonsArray = ArraysExercises.addPerson(persons, new Person("Jay"));
+     for(Person person: newPersonsArray){
+     person.sayHello();
+     }
+     //override toString in person.java
+     sout(Arrays.toString(newPersonsArray));
+
      **/
+//        Server Name Generator
+//
+//        We are going to build a server name generator.
+//        Create a class inside of src named ServerNameGenerator,
+//        and follow the specs below.
+//
+//        Create two arrays whose elements are strings:
+//        one with at least 10 adjectives, another with at
+//        least 10 nouns.
+//       Create a method that will return a random
+//       element from an array of strings.
+//       Add a main method, and inside of your main
+//       method select and random noun and adjective
 
     }
+
+    /** assuming you have person.java: do this out of main.
+     public static Person[] addPerson(Person[] persons, Person n00b){
+     Person[] newBiggerArray = new Person[persons.length + 1];
+     for (int i = 0; i < persons.length; i++){
+     newBiggerArray[i] = persons[i];
+     }
+     newBiggerArray[newBiggerArray.length-1] = n00b;
+     return newBiggerArray;
+     } **/
+
+    //optional movie list, try to make server name generator.
 }
