904b7a8d3a9553cb1ce6bf74b391cf0e86179f98
New Author Name <dennise.e.camacho@gmail.com>
2022-04-04 14:19:44 -0500

    Inheritance and polymorphism review

--- src/BirdTest.java
+++ src/BirdTest.java
@@ -1,5 +1,9 @@
 public class BirdTest {
     public static void main(String[] args) {
 
+        Duck mallard = new Duck();
+        mallard.setName("Mallard Duck");
+        mallard.makeNoise();
+
     }
 }
---
+++ src/Duck.java
@@ -0,0 +1,5 @@
+public class Duck extends Bird{
+    //everything in Bird extends to duck.
+    //everything in the bird class is accessible to the Duck class
+    //because of extends.
+}
