65b6c79410eef82aee6e3224cc1ce2fcb620841e
New Author Name <dennise.e.camacho@gmail.com>
2022-04-04 11:11:56 -0500

    Arrays exercises section 1 progress...

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,29 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class ArraysExercises {
+    public static void main(String[] args) {
+//        Array Basics
+//
+//        Create a class inside of src named ArraysExercises.
+//        Create a main method for this class to do your work.
+//
+//      What happens when you run the following code? Why is
+//      Arrays.toString necessary? it shows the array, not the #
+      int[] numbers = {1, 2, 3, 4, 5};
+      System.out.println("without Arrays.toString(): " +
+              numbers +
+              "\n" +
+              "with Arrays.toString(): " + Arrays.toString(numbers));
+//      Create an array that holds 3 Person objects. Assign a new
+//      instance of the Person class to each element. Iterate through
+//      the array and print out the name of each person in the array.
+//
+//      Create a static method named addPerson. It should
+//      accept an array of Person objects, as well as a
+//      single person object to add to the passed array. It
+//      should return an array whose length is 1 greater than
+//      the passed array, with the passed person object at
+//      the end of the array.
+    }
 }
