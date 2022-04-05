da7932a61ec72bb86999c580dac5f9ed99ffc88b
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 15:32:35 -0500

    methods for getting the length and width.

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,14 +1,24 @@
 package shapes;
 
 public abstract class Quadrilateral extends Shapes implements Measurable{
-    protected double length;
+
     protected double width;
+    protected double length;
 
     public Quadrilateral(){}
 
     public Quadrilateral(double length, double width){
         this.length = length;
         this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+
+    public double getWidth() {
+        return width;
     }
 
 
