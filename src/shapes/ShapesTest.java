2e3287477953f8776e08b4db28f9a9b2b1724dbe
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 17:46:58 -0500

    declare a variable of the type Measurable named myShape.

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,10 +1,14 @@
 package shapes;
 
-//public class ShapesTest {
+public class ShapesTest {
+    //declare a variable of the type Measurable named myShape.
+
+    Measurable myShape;
+
 ////    public static void main(String[] args) {
 ////
 ////        Rectangle box1 = new Rectangle(2,4);
 ////        System.out.println(box1.getPerimeter());
 ////        System.out.println(box1.getArea());
 //    }
-//}
+}
