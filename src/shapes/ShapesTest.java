acc51b466b0ec9fc08944934aa0e2ee831ed3572
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 13:52:29 -0500

    inheritance-and-polymorphism exercises, come back and review later

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,2 +1,12 @@
-package Shapes;public class ShapesTest {
+package shapes;
+
+import java.awt.Rectangle;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+
+        Rectangle box1 = new Rectangle(2,4);
+        System.out.println(box1.getPerimeter());
+        System.out.println(box1.getArea());
+    }
 }
