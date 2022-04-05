e035a986afd7177c47862b54bcf46dc0834a7428
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 15:55:50 -0500

    Square class to extend Quadrilateral.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,25 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double setLength() {
+        return 0;
+    }
+
+    @Override
+    public double setWidth() {
+        return 0;
+    }
 
 //    public Square(int length, int width) {
 //        super(length, width);
