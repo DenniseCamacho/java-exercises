c1a886f1d81ae741d0ca5421cbd7f8db9ce8e075
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 13:53:03 -0500

    inheritance-and-polymorphism exercises, come back and review later

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,30 @@
-package Shapes;
+package shapes;
 
-public class Squares extends Rectangle {
+public class Square extends Rectangle {
 
-public Square(){
-    
+
+
+    public Square(int length, int width) {
+        super(length, width);
+    }
+
+    public Square(int side){
+    super.length = side;
+    super.width = side;
+//     super(side, side);
+
 }
+
+public int getPerimeter(){
+        return 4 * super.width;
 }
+
+public int getArea(){
+        return (int) Math.pow(super.length, 2);
+}
+
+
+
+}
+//constructor
+// https://java.codeup.com/java-ii/object-oriented-programming/#constructors
