ee2770ee90f98471d006e4e1cbcf56348b53eeb0
New Author Name <dennise.e.camacho@gmail.com>
2022-04-05 15:30:12 -0500

    a constructor that accepts two numbers for the length and width and sets those properties.

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -4,5 +4,12 @@ public abstract class Quadrilateral extends Shapes implements Measurable{
     protected double length;
     protected double width;
 
+    public Quadrilateral(){}
+
+    public Quadrilateral(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
 
 }
