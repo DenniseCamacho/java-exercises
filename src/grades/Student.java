18081d0385ebe287e438eeb045b874cc8700f50c
New Author Name <dennise.e.camacho@gmail.com>
2022-04-06 15:33:16 -0500

    Test your Student class by adding a main method and creating Student objects.

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,12 +1,56 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+
     private String name;
-    private Double grade;
+    private ArrayList<Integer> grades;
 
+    public Student(){}
 
-    public Student(String name, Double grade) {
+    public Student(String name){
         this.name = name;
-        this.grade = grade;
+        this.grades = new ArrayList<>();
+
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+      grades.add(grade);
+    }
+
+
+    public double getGradeAverage(){
+        double sum = 0;
+        int theLength = grades.size();
+        for (int grade : grades){
+            sum += grade;
+        }
+        return sum / theLength;
+        //https://dirask.com/posts/Java-sum-all-ArrayList-elements-jm3PZD
     }
 }
+
+
+
+
+//        Create a Student class
+//
+//        Create a class named Student. It should have private properties for the student's name, and grades. The grades property should be an ArrayList of integers. The student class should have a constructor that sets name property, and initializes the grades property as an empty ArrayList. The Student class should have the following methods:
+//
+//
+//// returns the student's name
+//        public String getName();
+//// adds the given grade to the grades property
+//        public void addGrade(int grade);
+//// returns the average of the students grades
+//        public double getGradeAverage();
---
+++ src/grades/StudentTest.java
@@ -0,0 +1,24 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student studentOne = new Student("Dennise");
+        studentOne.addGrade(70);
+        studentOne.addGrade(60);
+        studentOne.addGrade(75);
+        studentOne.addGrade(89);
+        studentOne.addGrade(90);
+        studentOne.addGrade(55);
+        System.out.println(studentOne.getGrades());
+
+
+
+
+
+
+
+    }
+}
