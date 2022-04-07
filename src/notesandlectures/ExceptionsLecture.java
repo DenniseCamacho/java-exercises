ad6c90e3cc117788cb69a0d2a79e610eca60eeb7
New Author Name <dennise.e.camacho@gmail.com>
2022-04-07 14:07:41 -0500

    Java try, catch, finally, exceptions.

---
+++ src/notesandlectures/ExceptionsLecture.java
@@ -0,0 +1,8 @@
+package notesandlectures;
+
+public class ExceptionsLecture {
+    public String maybeAnInteger = "42";
+    public String defNotAnInteger = "I am NOT an integer";
+    public String testString = "PizzaBox";
+
+}
---
+++ src/notesandlectures/ExceptionsLectureTest.java
@@ -0,0 +1,51 @@
+package notesandlectures;
+
+public class ExceptionsLectureTest {
+
+    public static void main(String[] args) {
+
+        ExceptionsLecture lecture = new ExceptionsLecture();
+        try {
+            System.out.println(lecture.testString.substring(5));
+            System.out.println(lecture.testString.substring(21));
+
+        } catch (StringIndexOutOfBoundsException anyvarUsuallyNFE) {
+            System.out.println("Did not work: " + anyvarUsuallyNFE);
+        } finally {
+            System.out.println("this always runs");
+        }
+
+
+
+        /**
+        try{
+    ExceptionsLecture lecture = new ExceptionsLecture();
+        System.out.println(Integer.parseInt(lecture.defNotAnInteger));
+        System.out.println(Integer.parseInt(lecture.maybeAnInteger));
+        } catch (NumberFormatException nfe) {
+            //in the event nfe occurs, instead of all the error messages
+            //generate this message.
+            System.out.println("Got exception: " + nfe);
+            //an exception is thrown, and you 'catch' it.
+            //'overriding' behavior of error.
+            //common exception:
+//            nfe.printStackTrace(); //what intellij does by default.
+        }
+        **/
+
+//  Mini-exercise: Investigate the substring method for String objects in the Java
+//  API. Find out what kind of exception it throws. Create a test class with a
+//  main method. Set up a try-catch block that defines a string and finds its
+//  length. Once you have the length of your string, modify your program so it
+//  creates a substring from your original string, but give it an index that
+//  doesn't exist so you get the error message.
+//
+//  Optional: play with the .getMessage and .printStackTrace methods.
+//
+//  Optional: what are some other ways you can get the same exception? Try to set
+//  up your program so as to produce and catch these exceptions.
+//https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/Integer.html
+
+    }
+
+}
