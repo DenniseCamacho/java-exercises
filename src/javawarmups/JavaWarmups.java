22a25a66a7d4245d74a43e20aa2119aa5986975c
New Author Name <dennise.e.camacho@gmail.com>
2022-04-07 09:33:11 -0500

    Java warmup exercises question 1

--- src/javawarmups/JavaWarmups.java
+++ src/javawarmups/JavaWarmups.java
@@ -1,8 +1,23 @@
-public class JavaWarmups {
-    public static void main(String[] args) {
+package javawarmups;
 
-    }
-//TODO:Java warmup 1:
+public class JavaWarmups {
+        //# Java Assessment Practice Problems
+        //
+        //1. Create a class called Practice with a main method.
+
+        //2. Create a public static method in main called *subtractTen* that takes one integer argument and returns that number minus 10.
+        public static int subtractTen(int oneInteger) {
+            return oneInteger - 10;
+        }
+        //3. Create a public static method *average* that takes an array of integers and returns their average as a double.
+
+        //4. Create a class called Pet with two String instance variables, name and type.
+        //5. Create getters and setters for the properties as well as a constructor that takes two strings and sets the name and type.
+        //6. Create a PetDog class that inherits from Pet.
+        //7. In PetDog, create a boolean instance variable, trained.
+        //8. Write a constructor for PetDog that sets its name, type, and trained properties.
+        //9. Write an instance method in PetDog  called isTrained that returns the boolean value of the trained property.
+
 /**
  *
  * //    public static String returnFirstCapitalLetter(String word){
---
+++ src/javawarmups/Practice.java
@@ -0,0 +1,11 @@
+package javawarmups;
+
+public class Practice extends JavaWarmups {
+    //1. Create a class called Practice with a main method.
+    public static void main(String[] args) {
+
+        //2. Create a public static method in main called *subtractTen* that takes one integer argument and returns that number minus 10.
+        System.out.println(subtractTen(23));
+
+    }
+}
