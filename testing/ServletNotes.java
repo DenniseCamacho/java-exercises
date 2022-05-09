cb7c1abf3bd6a80a17140b732394198895104eea
New Author Name <dennise.e.camacho@gmail.com>
2022-05-08 22:19:02 -0500

    Servlet notes

---
+++ testing/ServletNotes.java
@@ -0,0 +1,17 @@
+
+<%--//--%>
+<%--//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/Hello")--%>
+<%--//public class HelloWorldServlet extends HttpServlet {--%>
+<%--//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {--%>
+<%--//        response.getWriter().println("<h1>Hello, World!</h1>");--%>
+<%--//    }--%>
+<%--//    protected void doPost(HttpServletRequest request, HttpServletResponse response)--%>
+<%--//            throws IOException, ServletException {--%>
+<%--//        String name = request.getParameter("name");--%>
+<%--//--%>
+<%--//        request.setAttribute("name", name);--%>
+<%--//        RequestDispatcher view = request.getRequestDispatcher("/hello.jsp");--%>
+<%--//        view.forward(request, response);--%>
+<%--//    }--%>
+<%--//}--%>
+
