34965bfb46dc6918cb497b4ebfaff847d7c38e1c
New Author Name <dennise.e.camacho@gmail.com>
2022-05-09 21:05:06 -0500

    Student class, with a main method in PersonTest creating Student objects. Verifying adding grades to each object, and that the getGradeAverage gets the average of the students grades.

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,22 +1,24 @@
+import java.util.ArrayList;
+
 public class PersonTest {
     public static void main(String[] args) {
-        Person person01 = new Person("Dennise");
-        person01.setName("Dennise");
-        person01.sayHello();
-        System.out.println("\nthis is person01: " + person01.getName());
-
-        Person person02 = new Person("Jan");
-        person02.setName("Jan");
-        person02.sayHello();
-        System.out.println("\nthis is person02: " + person02.getName());
-
-
-        System.out.printf(
-                          "\nperson01.getName() " +
-                          ( person01.getName() )  +
-                          "\nperson02.getName() " +
-                          ( person02.getName() )
-                            );
+//        Person person01 = new Person("Dennise");
+//        person01.setName("Dennise");
+//        person01.sayHello();
+//        System.out.println("\nthis is person01: " + person01.getName());
+//
+//        Person person02 = new Person("Jan");
+//        person02.setName("Jan");
+//        person02.sayHello();
+//        System.out.println("\nthis is person02: " + person02.getName());
+//
+//
+//        System.out.printf(
+//                          "\nperson01.getName() " +
+//                          ( person01.getName() )  +
+//                          "\nperson02.getName() " +
+//                          ( person02.getName() )
+//                            );
         //why are both names Jan?
         //person01 isnt Jan...
 
@@ -31,8 +33,12 @@ public class PersonTest {
 //        System.out.println(person1.getName());
 //        System.out.println(person2.getName());
 
-
-
-
+Student Denny = new Student("Dennise", new ArrayList<>(4));
+        Denny.addGrade(70);
+        Denny.addGrade(80);
+        Denny.addGrade(90);
+        Denny.addGrade(50);
+        Denny.addGrade(60);
+        System.out.println(Denny.getGradeAverage());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+private ArrayList<Integer> grades;
+
+   public Student(){
+   }
+    public Student(String name, ArrayList<Integer> grades) {
+        this.name = name;
+        this.grades = new ArrayList<>(grades);
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    };
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    };
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        float length = grades.size();
+        float sum = 0;
+        int i = 0;
+        while (i < length) {
+            sum += grades.get(i);
+            i++;
+        }
+        System.out.println(length);
+        return sum/length;
+    }
+
+}
