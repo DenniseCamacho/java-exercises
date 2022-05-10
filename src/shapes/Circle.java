0502001a1a8168ed5ffad57a118550dfa88646bd
New Author Name <dennise.e.camacho@gmail.com>
2022-05-09 19:18:15 -0500

    various methods for getting information about the circle

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,2 +1,26 @@
-package shapes;public class Circle {
+package shapes;
+
+public class Circle {
+    private int area;
+    private int circumference;
+    private int radius;
+    private float length;
+    private float width;
+    private double pi = Math.PI;
+    Circle(){}
+
+    public Circle(double radius) {
+        this.radius = (int) radius;
+    }
+
+    public double getArea() {
+        this.area = (int) (pi * (this.radius^2));
+        return area;
+    }
+
+    public int getCircumference() {
+        this.circumference = (int) (2 * pi * this.radius);
+        return circumference;
+    }
+
 }
