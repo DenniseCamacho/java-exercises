d6af5d5adadcc66537e8e8387883a66e5ce1d793
New Author Name <dennise.e.camacho@gmail.com>
2022-07-07 19:03:08 -0500

    attempting Java problem

--- src/redo/RedoControlFlow.java
+++ src/redo/RedoControlFlow.java
@@ -1,20 +1,47 @@
 package redo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RedoControlFlow {
 //    Create an integer variable i with a value of 5.
-    int i = 5;
-//    Create a while loop that runs so long as i is less than or equal to 15
-//    Each loop iteration, output the current value of i, then increment i by one.
-//    Your output should look like this:
-//
-//            5 6 7 8 9 10 11 12 13 14 15
+    private int fiveToFifteen = 5;
+    private int fromOneToHundred = 1;
+    private Map<String, List> maplist = new HashMap<>();
+    private ArrayList<String> strings = new ArrayList<>();
+    private ArrayList<Integer> integers = new ArrayList<>();
 public static void main(String[] args) {
-    RedoControlFlow i = new RedoControlFlow();
+    RedoControlFlow number = new RedoControlFlow();
 
-    do {
-        System.out.println(i.i++);
+//    do {
+//        System.out.println(i.i++);
+//    } while (i.i <= 15);
+    //[x]Write a program that prints the numbers from 1 to 100.
+//    do {
+//        System.out.println(number.fromOneToHundred++);
+//    } while (number.fromOneToHundred <= 100);
+    //[]For multiples of three: print “Fizz” instead of the number.
 
-    } while (i.i <= 15);
+    for (int i = 0; i < 100; i++) {
+        number.fromOneToHundred++;
+    if (number.fromOneToHundred % 3 == 0) {
+        number.strings.add("fizz");
+    } else {
+        number.integers.add(number.fromOneToHundred);
+    }
+    number.maplist.put(String.valueOf(number.strings), number.integers);
+    }
+
+
+    System.out.println(number.maplist);
+
+//    for (int i = 0; i < 100; i += 1) {
+//        System.out.println(number.fromOneToHundred++);
+//        if (number.fromOneToHundred % 3 == 0)
+//            number.stringIntArray.put("fizz", number.fromOneToHundred);
+//    }
 
 
 
